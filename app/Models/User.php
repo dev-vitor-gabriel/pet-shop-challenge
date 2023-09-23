9b9aa38355214948ecb9a1270b95f0d8e68c383d
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-09-23 17:41:00 -0300

    feat: add new column;

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable implements JWTSubject
     protected $fillable = [
         'name',
         'email',
-        'permission',
+        'is_admin',
         'password',
     ];
 
