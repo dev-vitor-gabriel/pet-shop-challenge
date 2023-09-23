447314ad52a4fb9890bb347ddc19149c4ffd7196
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-09-23 03:35:09 -0300

    feat: create table atendimento;

---
+++ database/migrations/2023_09_23_033203_create_tb_atendimento_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tb_atendimento', function (Blueprint $table) {
+            $table->id('id_atendimento_tba');
+            $table->unsignedBigInteger('id_pet_tba');
+            $table->foreign('id_pet_tba')->references('id_pet_tbp')->on('tb_pet');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tb_atendimento');
+    }
+};
