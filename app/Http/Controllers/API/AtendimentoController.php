dc6b7d4a5923473a3015c98e77a0b0921b1a7855
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-09-23 17:39:08 -0300

    feat: add validations in Atendimento Controller;

--- app/Http/Controllers/API/AtendimentoController.php
+++ app/Http/Controllers/API/AtendimentoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Atendimento;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 
 class AtendimentoController extends Controller
 {
@@ -14,31 +16,59 @@ class AtendimentoController extends Controller
             'id_pet_tba' => 'required|int'
         ]);
             $pet = Atendimento::create([
-                'id_pet_tba' => $request->id_pet_tba,
+                'id_pet_tba'          => $request->id_pet_tba,
+                'dta_atendimento_tba' => $request->dta_atendimento_tba
             ]);
 
         return response()->json($pet,201);
     }
 
-
     public function get(Int $id_atendimento = null) {
+        $is_admin = auth()->user()->is_admin;
+        $id_user  = Auth::id();
+
+        if($is_admin){
+            $id_user = null;
+        }
+
         if($id_atendimento){
-            $data = Atendimento::getById(($id_atendimento));
+            $data = Atendimento::getById($id_atendimento, $id_user);
             return $data;
         }
-        $data = Atendimento::getAll();
+
+        $data = Atendimento::getAll($id_user);
         return $data;
     }
 
     public function update(Int $id_atendimento, Request $request) {
+        $is_admin = auth()->user()->is_admin;
+        $id_user  = Auth::id();
+
+        if($is_admin){
+            $id_user = null;
+        }
         $request->validate([
             'id_pet_tba' => 'required|int'
-        ]); 
-        Atendimento::updateReg($id_atendimento, $request);
+        ]);
+        $atendimento = Atendimento::updateReg($id_atendimento, $request, $id_user);
+
+        if($atendimento == false){
+            return response()->json(null,404);
+        }
     }
 
-    // delete (inactivate)
     public function delete(Int $id_atendimento) {
-        Atendimento::deleteReg($id_atendimento);
+        $is_admin = auth()->user()->is_admin;
+        $id_user  = Auth::id();
+
+        if($is_admin){
+            $id_user = null;
+        }
+
+        $atendimento = Atendimento::deleteReg($id_atendimento, $id_user);
+
+        if($atendimento == false){
+          return response()->json(null,404);
+        }
     }
 }
