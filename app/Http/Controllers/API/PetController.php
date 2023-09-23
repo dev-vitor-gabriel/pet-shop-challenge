68ea70f289f5758b74aeb79b9391360c8e0eefe6
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-09-23 17:39:43 -0300

    feat: create validate and permission ;

--- app/Http/Controllers/API/PetController.php
+++ app/Http/Controllers/API/PetController.php
@@ -10,39 +10,70 @@ use Illuminate\Support\Facades\Auth;
 
 class PetController extends Controller
 {
-    public function create(Request $request) {
+    public function create(Request $request)
+    {
 
         $request->validate([
             'des_pet_tbp' => 'required|string|max:255'
         ]);
-            $pet = Pet::create([
-                'des_pet_tbp' => $request->des_pet_tbp,
-                'id_cliente_tbp'=> Auth::id(),
-                'is_ative_tbp' => 1,
-            ]);
+        $pet = Pet::create([
+            'des_pet_tbp' => $request->des_pet_tbp,
+            'id_cliente_tbp' => Auth::id(),
+            'is_ative_tbp' => 1,
+        ]);
 
-        return response()->json($pet,201);
+        return response()->json($pet, 201);
     }
 
 
-    public function get(Int $id_pet = null) {
-        if($id_pet){
-            $data = Pet::getById(($id_pet));
+    public function get(Int $id_pet = null)
+    {
+        $is_admin = auth()->user()->is_admin;
+        $id_user  = Auth::id();
+
+        if ($is_admin) {
+            $id_user = null;
+        }
+        if ($id_pet) {
+            $data = Pet::getById($id_pet, $id_user);
             return $data;
         }
-        $data = Pet::getAll();
+        $data = Pet::getAll($id_user);
         return $data;
     }
 
-    public function update(Int $id_pet, Request $request) {
+    public function update(Int $id_pet, Request $request)
+    {
+        $is_admin = auth()->user()->is_admin;
+        $id_user  = Auth::id();
+
+        if ($is_admin) {
+            $id_user = null;
+        }
         $request->validate([
             'des_pet_tbp' => 'required|string|max:255'
-        ]); 
-        Pet::updateReg($id_pet, $request);
+        ]);
+        $pet = Pet::updateReg($id_pet, $request, $id_user);
+
+        if ($pet == false) {
+            return response()->json(null, 404);
+        }
     }
 
     // delete (inactivate)
-    public function delete(Int $id_pet) {
-        Pet::deleteReg($id_pet);
+    public function delete(Int $id_pet)
+    {
+        $is_admin = auth()->user()->is_admin;
+        $id_user  = Auth::id();
+
+        if ($is_admin) {
+            $id_user = null;
+        }
+
+        $pet = Pet::deleteReg($id_pet,$id_user);
+
+        if ($pet == false) {
+            return response()->json(null, 404);
+        }
     }
 }
