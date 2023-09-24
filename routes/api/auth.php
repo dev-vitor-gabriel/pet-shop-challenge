c90df7d56471aac7fb15d147ff99407842a3df04
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-09-24 16:54:12 -0300

    feat: create new routes;

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 
 Route::controller(AuthController::class)->group(function () {
+    Route::get('{id}', 'get');
+    Route::get('', 'get');
     Route::post('login', 'login');
     Route::post('register', 'register');
     Route::post('logout', 'logout');
     Route::post('refresh', 'refresh');
+    Route::put('{id}', 'update');
+    Route::delete('{id}', 'delete');
 });
