d297c610bd0f7f7984552fb5c60301cd0c99b7b5
dev-vitor-gabriel <dev.vitor.gabriel@gmail.com>
2023-09-24 16:54:41 -0300

    feat: add new methods;

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,11 +8,12 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Facades\Hash;
+
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, HasRoles, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -44,6 +45,47 @@ class User extends Authenticatable implements JWTSubject
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function getAll($id_user)
+    {
+        $data = User::select(['*']);
+        if ($id_user != null) {
+            $data = $data->where('users.id', $id_user);
+        }
+        $data = $data->get();
+
+        return response()->json($data);
+    }
+
+    public static function updateReg(Int $id, $obj, $id_user)
+    {
+        $data = User::where('id', $id);
+        if ($id_user != null) {
+            $data = $data->where('users.id', $id_user);
+        }
+        $arrayColumns = [];
+        if (isset($obj->name))
+            $arrayColumns['name'] = $obj->name;
+        if (isset($obj->email))
+            $arrayColumns['email'] = $obj->email;
+        if (isset($obj->password))
+            $arrayColumns['password'] = Hash::make($obj->password);
+        if (isset($obj->is_admin))
+            $arrayColumns['is_admin'] = $obj->is_admin ?? false;
+        $data = $data->update($arrayColumns);
+        return $data;
+    }
+
+    public static function deleteReg($id, $id_user)
+    {
+        $data = User::where('id', $id);
+        if ($id_user != null) {
+            $data = $data->where('users.id', $id_user);
+        }
+        $data = $data->delete();
+
+        return $data;
+    }
 
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
